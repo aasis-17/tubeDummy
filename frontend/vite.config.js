76c6a568da3f9b208e7f16d2bb977f08a73d80dd
aasis-17 <dazlerboi@gmail.com>
2024-11-30 12:20:35 +0545

    testing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server : {
     proxy : {
-    "api" :  "https://tubedummy-backend.onrender.com"
+    "url" :  "https://tubedummy-backend.onrender.com"
     }
   },
   plugins: [react()],
