1bfb85efc96a4c1fa0cbbe2c1353922f56ad033c
aasis-17 <dazlerboi@gmail.com>
2024-12-07 17:27:56 +0545

    bug

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -6,7 +6,7 @@ import jwt from "jsonwebtoken"
 export const verifyJWT = asyncHandler(async(req, _, next) => {
 
 try {
-        const token = await req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+        const token = await req.cookies["accessToken"] || req.header("Authorization")?.replace("Bearer ", "")
 
         if(!token){
             throw new ApiError(401, "unauthorized request!!")
